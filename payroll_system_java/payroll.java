aa1b7434d8b907b85d85e90907e5014e6f355202
Franzoli-GitHub <Numbssys@gmail.com>
2024-09-19 22:09:22 +0800

    starting again

---
+++ payroll_system_java/payroll.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.random.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class payroll {
+    public static void main(String[] args) {
+        // starting from scratch
+    }
+}
