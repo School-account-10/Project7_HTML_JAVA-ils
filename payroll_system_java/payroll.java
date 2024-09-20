99e6e870619d57109a117a41e5a67d833c705fd9
Franzoli-GitHub <Numbssys@gmail.com>
2024-09-20 10:05:05 +0800

    Update payroll.java

--- payroll_system_java/payroll.java
+++ payroll_system_java/payroll.java
@@ -5,6 +5,28 @@ import java.util.List;
 
 public class payroll {
     public static void main(String[] args) {
-        // starting from scratch
+        // make a variable to call scanner
+        boolean T_f = false;
+        Scanner New_scanner1 = new Scanner(System.in);
+        // asking for user information
+        System.out.println("Developer payroll page");
+
+        System.out.print("name: ");
+        String name = New_scanner1.nextLine();
+        System.out.print("age: ");
+        int age;
+        while (!T_f) {
+            if (New_scanner1.hasNextInt()) {
+                System.out.println("printing here1");
+                break;
+
+            } else {
+                New_scanner1.next();
+                System.out.println("printing here2");
+                continue;
+            } if (new);
+
+        }
+
     }
 }
