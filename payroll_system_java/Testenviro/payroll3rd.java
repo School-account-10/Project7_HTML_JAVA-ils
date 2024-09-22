1a8dc812d7d7afee8898402f2a89ed2ac5ef20b5
Franzoli-GitHub <Numbssys@gmail.com>
2024-09-22 14:56:34 +0800

    trying again

--- payroll_system_java/Testenviro/payroll3rd.java
+++ payroll_system_java/Testenviro/payroll3rd.java
@@ -1,15 +1,53 @@
 package Testenviro;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.random.*;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-
 
 public class payroll3rd {
     public static void main(String[] args) {
-    
+        boolean tr_fl = false; // true and false current value false
+
+        Scanner new_scanner1 = new Scanner(System.in);
+        ArrayList<String> Listofdepartment = new ArrayList<String>();
+        Listofdepartment.add("Human Resources");
+        Listofdepartment.add("Finance");
+        Listofdepartment.add("Marketing");
+        Listofdepartment.add("Sales");
+        Listofdepartment.add("Operations");
+        int department1 = 0;
+        int counter1;
+
+        System.out.print("Remaining departments: ");
+        System.out.println(Listofdepartment.size());
+        System.out.print("How many departments Do You Want to create: ");
+
+        while (!tr_fl) {
+            try {
+                department1 = new_scanner1.nextInt();
+            } catch (InputMismatchException e) {
+                new_scanner1.next();
+                System.out.println("Numbers Only Please try again");
+                System.out.print("How many departments Do You Want to create: ");
+                continue;
+
+            }
+            if (Listofdepartment.size() < department1) {
+                System.out.println("bigger than list selected please try again ");
+                System.out.print("How many departments Do You Want to create: ");
+            } else {
+                System.out.println("working ");
+                for(counter1 = 0; counter1 < department1; counter1++){
+                Listofdepartment.remove(Listofdepartment.size() -1 );
+                System.out.println(Listofdepartment.get(counter1));
+                }
+                
+            }
+
+        }
+        
+
     }
-    
 }
---
+++ payroll_system_java/Testenviro/payroll4th.java
@@ -0,0 +1,5 @@
+package Testenviro;
+
+public class payroll4th {
+    
+}
