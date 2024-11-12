c2d70a005f592a9c18faefcb7a19340889032f58
Franzoli-GitHub <Numbssys@gmail.com>
2024-11-12 21:36:56 +0800

    Update payroll4th.java

--- payroll_system_java/Testenviro/payroll4th.java
+++ payroll_system_java/Testenviro/payroll4th.java
@@ -1,5 +1,9 @@
 package Testenviro;
 
 public class payroll4th {
-    
+    public static void main (String [] args){
+        for(int x = 3; x >= 0; x--){
+            System.out.println(x);
+        }
+    }
 }
