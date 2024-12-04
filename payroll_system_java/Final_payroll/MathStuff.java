a08ddaa1c99869ebed16591d058bd552c0d401e9
Franzoli-GitHub <Numbssys@gmail.com>
2024-12-04 16:15:16 +0800

    h

--- payroll_system_java/Final_payroll/MathStuff.java
+++
@@ -1,9 +0,0 @@
-package Final_payroll;
-import java.util.Scanner;
-import java.util.random.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-public class MathStuff {
-    
-}
---
+++ payroll_system_java/Final_payroll/payroll.java
@@ -0,0 +1,16 @@
+import com.sun.nio.sctp.PeerAddressChangeNotification;
+import java.util.ArrayList;
+import java.lang.Math;
+import java.util.Scanner;
+
+public class payroll {
+
+  public static void main(String[] args) {
+    Scanner nc1 = new Scanner(System.in);
+    String departmentName;
+    ArrayList<ArrayList<String>> departments = new ArrayList<ArrayList<String>>();
+    System.out.print("Insert Department name: ");
+    
+    
+  } 
+}
